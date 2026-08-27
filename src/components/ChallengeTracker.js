bd6b50fc4666cc206c5093b112f4203b263d5a1f
112112112112112 <kimika1863@gmail.com>
2026-08-27 13:34:22 +0200

    render both challenge dungeons

--- src/components/ChallengeTracker.js
+++ src/components/ChallengeTracker.js
@@ -5,8 +5,8 @@ import { Button, Form } from "react-bootstrap";
 export default function ChallengeTracker({characters, checklist, setChecklist, tasks}) {
     const [challengeData, setChallengeData] = useState(null);
     const [currentWeek, setCurrentWeek] = useState('');
+    const [viewDungeon, setViewDungeon] = useState(null);
     const challengeTask = tasks.find(t => t.title === 'Challenge Mode');
-
 
     useEffect(() => {
         async function load() {
@@ -32,22 +32,29 @@ export default function ChallengeTracker({characters, checklist, setChecklist, t
         load();
     }, [characters]);
 
-    return (
-        <>
-            <h2><img src={`/img/tasks/${currentWeek.toLowerCase()}.webp`} alt={currentWeek} width="60px"/> Challenge Tracker</h2>
+    const displayDungeon = viewDungeon || currentWeek;
+
+    const renderDungeonTable = (dungeon) => {
+        const getAura = (charData) => {
+            return dungeon === 'Rosso' ? charData.rossoAura : charData.bertheAura;
+        };
+
+        const isActive = dungeon === currentWeek;
+
+        return (
             <table className='text-center box'>
                 <thead>
                     <tr>
                         <th colSpan={2}>Character</th>
-                        <th><img src={`img/items/${currentWeek.toLowerCase()}-aura.webp`} alt={`${currentWeek} Aura`} /></th>
-                        <th><img src={`img/items/${currentWeek.toLowerCase()}-reset.webp`} alt={`${currentWeek} Reset Tickets`} /></th>
+                        <th><img src={`img/items/${dungeon.toLowerCase()}-aura.webp`} alt={`${dungeon} Aura`} /></th>
+                        <th><img src={`img/items/${dungeon.toLowerCase()}-reset.webp`} alt={`${dungeon} Reset Tickets`} /></th>
                         <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {characters.map(c => {
                         const charData = challengeData?.characters?.[c.id] || {rossoAura: 0, bertheAura: 0, resetTicketUsed: 0};
-                        const aura = currentWeek === 'Rosso' ? charData.rossoAura : charData.bertheAura;
+                        const aura = getAura(charData);
                         const isCleared = checklist.some(cl => cl.character_id === c.id && cl.task_id === challengeTask?.id && cl.completed === 1);
 
                         return (
@@ -55,72 +62,72 @@ export default function ChallengeTracker({characters, checklist, setChecklist, t
                                 <td>
                                     <img src={`/img/classes/${c.class}.png`} style={{ width: '32px', height: '32px', objectFit: 'contain' }} />
                                 </td>
-                                <td>
-                                    {c.name}
-                                </td>
+                                <td>{c.name}</td>
                                 <td>{aura}</td>
                                 <td>{charData.resetTicketUsed}/2</td>
                                 <td>
                                     <div className="button-box d-flex justify-content-center">
-                                        {!isCleared ? (
+                                        {isActive && !isCleared && (
                                             <Button
                                                 size="sm"
                                                 className="mx-2"
                                                 variant="outline-light"
                                                 onClick={async () => {
-                                                    await db.updateAura(c.id, currentWeek, 30);
+                                                    await db.updateAura(c.id, dungeon, 30);
                                                     await db.updateChecklist(c.id, challengeTask.id, 1);
                                                     const data = await db.getChallengeData();
-
                                                     setChallengeData(data);
                                                     const updatedChecklist = await db.getChecklist();
                                                     setChecklist(updatedChecklist);
                                                 }}
                                             >
                                                 Clear (+30 Aura)
-                                                </Button>
-                                        ) : (
-                                            <></>
+                                            </Button>
                                         )}
+                                        
+                                        {/* exchanges, set & reset are always visible */}
                                         <Button
                                             size="sm"
                                             className="mx-2"
                                             variant="outline-light"
                                             disabled={aura < 90}
                                             onClick={async () => {
-                                                await db.setAura(c.id, currentWeek, aura -90);
-                                                const charData = await db.getChallengeData();
-                                                setChallengeData(charData);
+                                                await db.setAura(c.id, dungeon, aura - 90);
+                                                const data = await db.getChallengeData();
+                                                setChallengeData(data);
                                             }}
                                         >
                                             Exchange Suit (-90 Aura)
                                         </Button>
+                                        
                                         <Button
                                             size="sm"
                                             className="mx-2"
                                             variant="outline-light"
-                                            disabled = {aura < 240}
+                                            disabled={aura < 240}
                                             onClick={async () => {
-                                                await db.setAura(c.id, currentWeek, aura -240);
-                                                const charData = await db.getChallengeData();
-                                                setChallengeData(charData);
+                                                await db.setAura(c.id, dungeon, aura - 240);
+                                                const data = await db.getChallengeData();
+                                                setChallengeData(data);
                                             }}
                                         >
                                             Exchange Force (-240 Aura)
                                         </Button>
+                                        
                                         <Button
                                             size="sm"
                                             className="mx-2"
                                             variant="outline-light"
                                             onClick={async () => {
-                                                await db.resetAura(c.id, currentWeek);
-                                                const charData = await db.getChallengeData();
-                                                setChallengeData(charData);
+                                                await db.resetAura(c.id, dungeon);
+                                                const data = await db.getChallengeData();
+                                                setChallengeData(data);
                                             }}
                                         >
                                             Reset Aura
                                         </Button>
-                                        {isCleared && (
+                                        
+                                        {isActive && isCleared && (
                                             <Button
                                                 size="sm"
                                                 className="mx-2"
@@ -135,8 +142,8 @@ export default function ChallengeTracker({characters, checklist, setChecklist, t
                                                     }
 
                                                     await db.updateChecklist(c.id, challengeTask.id, 0);
-                                                    const charData = await db.getChallengeData();
-                                                    setChallengeData(charData);
+                                                    const data = await db.getChallengeData();
+                                                    setChallengeData(data);
 
                                                     const updatedChecklist = await db.getChecklist();
                                                     setChecklist(updatedChecklist);
@@ -146,10 +153,8 @@ export default function ChallengeTracker({characters, checklist, setChecklist, t
                                             </Button>
                                         )}
 
-
                                         <div className="d-flex align-items-center">
                                             <Form.Control
-                                                // autoFocus
                                                 type="number"
                                                 size="sm"
                                                 className="mx-2"
@@ -159,9 +164,9 @@ export default function ChallengeTracker({characters, checklist, setChecklist, t
                                                     if (e.key === 'Enter') {
                                                         const value = parseInt(e.target.value);
                                                         if (!isNaN(value) && value >= 0) {
-                                                            await db.setAura(c.id, currentWeek, value);
-                                                            const charData = await db.getChallengeData();
-                                                            setChallengeData(charData);
+                                                            await db.setAura(c.id, dungeon, value);
+                                                            const data = await db.getChallengeData();
+                                                            setChallengeData(data);
                                                             e.target.value = '';
                                                         }
                                                     }
@@ -175,6 +180,44 @@ export default function ChallengeTracker({characters, checklist, setChecklist, t
                     })}
                 </tbody>
             </table>
+        );
+    };
+
+    return (
+        <>
+            <div className="d-flex justify-content-between align-items-center">
+                <h2>
+                    <img src={`/img/tasks/${displayDungeon.toLowerCase()}.webp`} alt={displayDungeon} width="60px"/>  {displayDungeon} Challenge Tracker
+                </h2>
+                <div className="d-flex gap-2">
+                     {currentWeek !== 'Berthe' && (
+                        <Button
+                            variant={viewDungeon === 'Berthe' ? 'outline-active' : 'outline-light'}
+                            size="sm"
+                            onClick={() => setViewDungeon('Berthe')}
+                        >
+                            View Berthe
+                        </Button>
+                    )}
+                    {currentWeek !== 'Rosso' && (
+                        <Button
+                            variant={viewDungeon === 'Rosso' ? 'outline-active' : 'outline-light'}
+                            size="sm"
+                            onClick={() => setViewDungeon('Rosso')}
+                        >
+                            View Rosso
+                        </Button>
+                    )}
+                    <Button
+                        variant={viewDungeon === null ? 'outline-active' : 'outline-light'}
+                        size="sm"
+                        onClick={() => setViewDungeon(null)}
+                    >
+                        🔄 Current Week ({currentWeek})
+                    </Button>
+                </div>
+            </div>
+            {renderDungeonTable(displayDungeon)}
         </>
     );
 }
